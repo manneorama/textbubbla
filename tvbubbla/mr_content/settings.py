9c8553115e57839eaebe955b3566a8a549ab7e14
Emanuel Birge <emanuel.birge@gmail.com>
2015-05-26 21:26:47 +0200

    it's called textbubbla, not tvbubbla, renaming

--- tvbubbla/mr_content/settings.py
+++
@@ -1,9 +0,0 @@
-
-class rex(object):
-    positive = 'article|body|content|entry|hentry|main|page|pagination|post|text|blog|story'
-    negative = 'comment|contact|foot|footer|footnote|meta|related|sidebar|menu|tags|widget'
-
-
-crap = [
-    'script', 'meta', 'link',
-]
